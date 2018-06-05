542675dd45b09f3184a6ccd3a48c46df87db7a1e
root <root@localhost.localdomain>
2018-06-05 04:07:23 -0400

    add session study py

---
+++ session_study.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import tensorflow as tf
+
+matrix1 = tf.constant([[3,3]])
+matrix2 = tf.constant([[2],
+		       [2]])
+
+product = tf.matmul(matrix1,matrix2) #matrix multiply no.dot(m1,m2)
+
+#two method to use session
+
+
+##method 1
+#sess = tf.Session()
+#result= sess.run(product)
+#print result
+#sess.close()
+
+
+#method 2 
+with tf.Session() as sess:
+   result2=sess.run(product)
+   print(result2)
+
+
+
+
